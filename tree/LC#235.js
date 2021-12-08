d19512be7d2748f11f9219fc84a7c5986d60211b
少白 陳 <shaobaichen@shaobai-chende-MacBook-Pro.local>
2021-12-09 01:02:58 +0800

    dp start

--- tree/LC#235.js
+++ tree/LC#235.js
@@ -13,11 +13,36 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function (root, p, q) {
-  //  recursion
-  if (q.val === root.val || p.val === root.val) return root;
+var lowestCommonAncestor_iter = function (root, p, q) {
+  //  iteration
+  const pVal = p.val;
+  const qVal = q.val;
+  let currNode = root;
 
-  if (p.val < root.val < q.val || p.val > root.val > q.val) return root;
+  while (currNode) {
+    let currVal = currNode.val;
+    if (pVal < currVal && qVal < currVal) {
+      currNode = currNode.left;
+    } else if (pVal > currVal && qVal > currVal) {
+      currNode = currNode.right;
+    } else {
+      return currNode;
+    }
+  }
+};
+
+var lowestCommonAncestor_recur = function (root, p, q) {
+  //  recursion
+  if (q.val === root.val || p.val === root.val) {
+    return root;
+  }
+
+  if (
+    (root.val < q.val && p.val < root.val) ||
+    (root.val > q.val && p.val > root.val)
+  ) {
+    return root;
+  }
 
   if (p.val < root.val && q.val < root.val) {
     return lowestCommonAncestor(root.left, p, q);
