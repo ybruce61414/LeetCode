3eb0633897e9a515dac2675ab63c268fb821f7a4
Viola <ybruce61414@gmail.com>
2021-12-09 18:52:51 +0800

    add greedy method to 55

--- dynamic-programming/LC#55.js
+++ dynamic-programming/LC#55.js
@@ -1,5 +1,35 @@
+//55. Jump Game
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function (nums) {};
+var canJump1 = function (nums) {
+  // dp method
+  let dpPositions = new Array(nums.length).fill(false);
+  dpPositions[0] = true;
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dpPositions[j] && j + nums[j] >= i) {
+        dpPositions[i] = true;
+        break;
+      }
+    }
+  }
+  return dpPositions[nums.length - 1];
+};
+
+var canJump2 = function (nums) {
+  // greedy method
+  if (nums.length === 1) return true;
+
+  let maxPosition = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxPosition) return false;
+    if (maxPosition >= nums.length - 1) return true;
+    maxPosition = Math.max(maxPosition, nums[i] + i);
+  }
+};
+
+// console.log(canJump1([3, 2, 1, 0, 4]));
+console.log(canJump2([2, 3, 1, 1, 4]));
