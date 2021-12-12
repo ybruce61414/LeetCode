d054a1e774fc6de65172bc6999bf499ab9d7a385
viola.chen <viola.chen@violachendeMacBook-Pro.local>
2021-12-13 00:21:00 +0800

    300 ologn

--- dynamic-programming/LC#300.js
+++ dynamic-programming/LC#300.js
@@ -3,5 +3,35 @@
  * @param {number[]} nums
  * @return {number}
  */
-// to-do nlogn : using binary search
-var lengthOfLIS = function (nums) {};
+
+var lengthOfLIS1 = function (nums) {
+  //  method1: dp
+  const dp = new Array(nums.length).fill(1);
+  let maxSoFar = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    maxSoFar = Math.max(maxSoFar, dp[i]);
+  }
+  return maxSoFar;
+};
+
+var lengthOfLIS2 = function (nums) {
+  // to-do nlogn : using binary search
+  let res = [nums[0]];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      res.push(nums[i]);
+    } else {
+      let repIndex = res.indexOf(nums[i]);
+      res[repIndex] = nums[i];
+    }
+  }
+};
+
+console.log(lengthOfLIS1([10, 9, 2, 5, 3, 7, 101, 18]));
--- utils/binarySearch.js
+++ utils/binarySearch.js
@@ -1,15 +1,15 @@
 const binarySearch = (nums, target) => {
   let midPosition = Math.floor(nums.length / 2);
 
-  if (target === nums[midPosition]) {
-    return target;
-  } else if (nums.length === 0) {
-    return undefined;
-  } else if (target > nums[midPosition]) {
-    return binarySearch(nums.slice(midPosition + 1, nums.length + 1), target);
+  if (nums[midPosition] === target) return target;
+
+  if (nums.length === 1) return undefined;
+
+  if (target > nums[midPosition]) {
+    return binarySearch(nums.slice(midPosition + 1, nums.length), target);
   } else {
     return binarySearch(nums.slice(0, midPosition), target);
   }
 };
 
-console.log(binarySearch([2, 5, 7, 8, 19, 50], 19));
+console.log(binarySearch([2, 5, 7, 8, 19, 50], 20));
