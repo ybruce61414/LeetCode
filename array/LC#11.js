ea49ea3781a70ef422a83dffe3e15a6ef17e9b54
少白 陳 <shaobaichen@shaobai-chende-MacBook-Pro.local>
2021-12-20 20:08:53 +0800

    121

---
+++ array/LC#11.js
@@ -0,0 +1,36 @@
+//11. Container With Most Water
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea1 = function (height) {
+  let max = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      let area = Math.min(height[i], height[j]) * (j - i);
+      max = Math.max(max, area);
+    }
+  }
+  return max;
+};
+
+var maxArea2 = function (height) {
+  // two pointer (kind of like greedy method)
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (right > left) {
+    const currArea = Math.min(height[left], height[right]) * (right - left);
+    maxArea = Math.max(max, currArea);
+
+    if (height[left] > height[right]) {
+      right = right - 1;
+    } else {
+      left = left + 1;
+    }
+  }
+  return maxArea;
+};
+
+console.log(maxArea2([4, 3, 2, 1, 4]));
---
+++ array/LC#121.js
@@ -0,0 +1,6 @@
+// 121. Best Time to Buy and Sell Stock
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {};
