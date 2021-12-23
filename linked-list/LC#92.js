b2d4f0bc4280f67783d6563471ca03739c0b924d
viola.chen <viola.chen@violachendeMacBook-Pro.local>
2021-12-24 00:29:53 +0800

    to do 92

---
+++ linked-list/LC#92.js
@@ -0,0 +1,15 @@
+// 92. Reverse Linked List II
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (head, left, right) {};
