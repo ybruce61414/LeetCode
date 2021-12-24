75ab5432ed22e821462405cabdc81f8e63005d0a
Viola <ybruce61414@gmail.com>
2021-12-24 18:53:05 +0800

    92 wip

--- linked-list/LC#92.js
+++ linked-list/LC#92.js
@@ -12,4 +12,42 @@
  * @param {number} right
  * @return {ListNode}
  */
-var reverseBetween = function (head, left, right) {};
+var reverseBetween = function (head, left, right) {
+  let pointer = head;
+  let count = 1;
+  let prev = null;
+  let before = null;
+  let after = null;
+  let rf;
+  let re;
+
+  if (!head.next) return head;
+
+  while (pointer) {
+    if (count === left - 1 && left - 1 > 0) before = pointer;
+    if (count === right + 1 && right + 1 > 0) after = pointer;
+
+    if (count === left) {
+      console.log("---left", pointer);
+      re = pointer;
+      prev = pointer;
+      pointer = pointer.next;
+      console.log("---leftprev", prev);
+    } else if (count > left && count <= right) {
+      if (count === right) rf = pointer;
+      console.log("-1---", count, pointer, prev);
+      let next = pointer.next;
+      pointer.next = prev;
+      prev = pointer;
+      pointer = next;
+    } else {
+      pointer = pointer.next;
+    }
+    count += 1;
+  }
+
+  if (before) before.next = rf;
+  if (after) re.next = after;
+
+  return !before ? prev : head;
+};
