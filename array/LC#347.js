fd404904be3d6f6f14398a1395d6fab4cdfb32e9
viola.chen <viola.chen@violachendeMacBook-Pro.local>
2021-12-27 00:38:33 +0800

    ready to do 347

---
+++ array/LC#347.js
@@ -0,0 +1,9 @@
+// 347. Top K Frequent Elements
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {};
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
