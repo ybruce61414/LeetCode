e0902c22ffb421de5deb34c94b76853ff8f43c2c
少白 陳 <shaobaichen@shaobai-chende-MacBook-Pro.local>
2021-12-27 20:29:37 +0800

    add lc#2

--- linked-list/LC#2.js
+++ linked-list/LC#2.js
@@ -11,4 +11,38 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {};
+var addTwoNumbers = function (l1, l2) {
+  let plus = Infinity;
+  let head;
+  let prev;
+  let ll1 = l1;
+  let ll2 = l2;
+
+  while (ll1 || ll2 || plus > 0) {
+    let sum =
+      (ll1?.val || 0) + (ll2?.val || 0) + (plus === Infinity ? 0 : plus);
+
+    plus = ~~(sum / 10);
+
+    if (!ll1 && !ll2) {
+      prev.next = new ListNode(sum);
+    }
+
+    if (ll1) {
+      prev = ll1;
+      ll1.val = sum % 10;
+      ll1 = ll1.next;
+      head = l1;
+    }
+
+    if (ll2) {
+      prev = ll2;
+      ll2.val = sum % 10;
+      ll2 = ll2.next;
+      head = l2;
+    }
+  }
+  return head;
+};
+
+console.log(~~(3 / 5));
