a5fb469ce80de53a7f10f9f24f1a8857a2b89ada
Viola <ybruce61414@gmail.com>
2022-01-06 18:55:11 +0800

    refactor 105

--- tree/LC#105.js
+++ tree/LC#105.js
@@ -12,4 +12,23 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function (preorder, inorder) {};
+var buildTree = function (preorder, inorder) {
+  if (preorder.length === 0 || inorder.length === 0) {
+    return null;
+  }
+
+  const node = new TreeNode(preorder[0]);
+
+  for (let i = 0; i < preorder.length; i++) {
+    if (preorder[0] === inorder[i]) {
+      node.left = buildTree(preorder.slice(1, i + 1), inorder.slice(0, i));
+      node.right = buildTree(
+        preorder.slice(i + 1, preorder.length),
+        inorder.slice(i + 1, inorder.length)
+      );
+      break;
+    }
+  }
+
+  return node;
+};
