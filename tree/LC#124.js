95eb8aef53398c0ed82adb245b90c06421ef80c0
Viola <ybruce61414@gmail.com>
2022-01-14 17:49:52 +0800

    organized 124

--- tree/LC#124.js
+++ tree/LC#124.js
@@ -27,3 +27,23 @@ var maxPathSum = function (root) {
   dfs(root);
   return max;
 };
+
+var maxPathSum1 = function (root) {
+  let maxSum = -Infinity;
+
+  const dfs = (node) => {
+    if (!node) return 0;
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    let innerMaxSum = Math.max(left, 0) + Math.max(right, 0) + node.val;
+    maxSum = Math.max(maxSum, innerMaxSum);
+
+    let outterMaxSum = Math.max(left, right) + node.val;
+    return Math.max(outterMaxSum, 0);
+  };
+
+  dfs(root);
+  return maxSum;
+};
